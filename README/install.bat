71536d0f63cb1e838d09e28dd519fadf03e17268
[lxhxwj] <[lxh_xwj@126.com]>
2018-12-18 17:16:41 +0800

    新建

---
+++ README/install.bat
@@ -0,0 +1,18 @@
+@echo off
+python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple requests
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple selenium
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple paramiko
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple xlrd
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple xlwt
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple python-docx
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple python-pptx
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pygame
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple canvas
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple reportlab
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple wmi
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple psutil
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple comtypes
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple configobj
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple configparser
+pause
